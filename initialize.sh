99cbc3851a275e447f504aedab8d9351111f37f6
SiberDave <dave.cloud.cyber@gmail.com>
2024-09-10 19:49:21 +0000

    Setup nextcloud app (not done)

--- initialize.sh
+++ initialize.sh
@@ -7,7 +7,9 @@ mkdir -p data/codeserver/config
 mkdir -p data/portainer
 
 cp env-template .env
+cp env-nextcloud-template .env-nextcloud
 
 ./self-sign-creator.sh local.vaultwarden
 ./self-sign-creator.sh local.code-server
 ./self-sign-creator.sh local.portainer
+./self-sign-creator.sh local.nextcloud
