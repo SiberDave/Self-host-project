7f1ab27eac1234d845e31298248097ffa6cf3674
SiberDave <dave.cloud.cyber@gmail.com>
2024-09-22 07:11:08 +0000

    Adding dnsmasq for dns server for tailscale dns and for accessing local domain from device linked on tailscale

--- initialize.sh
+++ initialize.sh
@@ -6,9 +6,11 @@ mkdir -p data/codeserver/workspace
 mkdir -p data/codeserver/config
 mkdir -p data/portainer
 mkdir -p data/prometheus
+mkdir -p data/dnsmasq
 
 cp prometheus/prometheus.yml data/prometheus/prometheus.yml
 cp nginx/nginx.conf data/nginx/nginx.conf
+cp dnsmasq/dnsmasq.conf data/dnsmasq/dnsmasq.conf
 
 cp codeserver/env-codeserver-template .env-codeserver
 cp nextcloud/env-nextcloud-template .env-nextcloud
@@ -16,6 +18,7 @@ cp vaultwarden/env-vaultwarden-template .env-vaultwarden
 cp postgresql/env-postgre-template .env-postgre
 cp mariadb/env-maria-template .env-maria
 cp gitea/env-gitea-template .env-gitea
+cp dnsmasq/env-dnsmasq-template .env-dnsmasq
 
 nginx/self-sign-creator.sh local.vaultwarden
 nginx/self-sign-creator.sh local.code-server
@@ -23,6 +26,7 @@ nginx/self-sign-creator.sh local.portainer
 nginx/self-sign-creator.sh local.nextcloud
 nginx/self-sign-creator.sh local.prometheus
 nginx/self-sign-creator.sh local.gitea
+nginx/self-sign-creator.sh local.dnsmasq
 # nginx/self-sign-creator.sh local.gitlab
 # nginx/self-sign-creator.sh local.webtop
 
